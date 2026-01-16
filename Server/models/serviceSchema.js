f33bc9b02bf6f34c0858d2a21142c635a344bcdc
Almujim Hadjili <almujim.hadjili@gmail.com>
2026-01-16 16:03:05 +0800

    fix: Update default value of queueLimit to null in serviceSchema

--- Server/models/serviceSchema.js
+++ Server/models/serviceSchema.js
@@ -5,7 +5,7 @@ const serviceSchema = new mongoose.Schema(
     name: { type: String, required: true },
     description: { type: String, required: true },
     identifier: { type: String, required: true },
-    queueLimit: { type: Number, default: 50 },
+    queueLimit: { type: Number, default: null },
   },
   { timestamps: true }
 );
